622f9ffbf8bfa795f30a6e9c01460046dcc6cf55
emmaajebit <126923735+emmaajebit@users.noreply.github.com>
2023-04-24 19:41:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def uploadToGCS(faces, file_name):
     f.write(str(faces))
     f.close()
 
-    face_bucket = storage_client.bucket("func-3-out")
+    face_bucket = storage_client.bucket("demo-bucket-1193")
     uploadfilename =  file_name + "-res.txt"
     new_blob = face_bucket.blob(uploadfilename)
     new_blob.upload_from_filename(temp_local_filename)
